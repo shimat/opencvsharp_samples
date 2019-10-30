32a66449e116c9ace882a495ff19634ba05ebd2b
vinicius121 <vinicio121@hotmail.com>
2019-10-30 13:42:30 +0000

    Update FaceDetectionDNN.cs

--- SamplesCS/Samples/FaceDetectionDNN.cs
+++ SamplesCS/Samples/FaceDetectionDNN.cs
@@ -15,8 +15,8 @@ namespace SamplesCS
         public void Run()
         {
             string finalOutput = "DetectedFaces.jpg";
-            string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
 	    string configFile = "deploy.prototxt";
+            string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
 	    var image = "faces.jpg";
 	    
             // Read sample image
