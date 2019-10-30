93df61af8fbd93a3c4d82be56963d1fbe045d93a
vinicius121 <vinicio121@hotmail.com>
2019-10-30 14:41:37 +0000

    Update FaceDetectionDNN.cs

--- SamplesCS/Samples/FaceDetectionDNN.cs
+++ SamplesCS/Samples/FaceDetectionDNN.cs
@@ -13,25 +13,26 @@ namespace SamplesCS
     class FaceDetectionDNN : ISample
     {
         public void Run()
-        {
-            string finalOutput = "DetectedFaces.jpg";
+        {            
+	    string configFile = "deploy.prototxt";
             string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
-			string configFile = "deploy.prototxt";
-			var image = "faces.jpg";
+	    string finalOutput = "DetectedFaces.jpg";
+	    var image = "faces.jpg";
+	    
             // Read sample image
             var frame = Cv2.ImRead(image);
             int frameHeight = frame.Rows;
             int frameWidth = frame.Cols;
             using (var faceNet = CvDnn.ReadNetFromCaffe(configFile, faceModel)) 
             {
-				using (Mat blob = CvDnn.BlobFromImage(frame, 1.0, new OpenCvSharp.Size(300, 300), new Scalar(104, 117, 123), false, false)) 
-				{
-					faceNet.SetInput(blob, "data");
+		using (Mat blob = CvDnn.BlobFromImage(frame, 1.0, new OpenCvSharp.Size(300, 300), new Scalar(104, 117, 123), false, false)) 
+		{
+		    faceNet.SetInput(blob, "data");
 
-					using (var detection = faceNet.Forward("detection_out")) 
-					{
-						using (Mat detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F, detection.Ptr(0))) 
-						{
+		    using (var detection = faceNet.Forward("detection_out")) 
+		    {
+			using (Mat detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F, detection.Ptr(0))) 
+			{
                             for (int i = 0; i < detectionMat.Rows; i++)
                             {
                                 float confidence = detectionMat.At<float>(i, 2);
@@ -42,16 +43,15 @@ namespace SamplesCS
                                     int y1 = (int)(detectionMat.At<float>(i, 4) * frameHeight);
                                     int x2 = (int)(detectionMat.At<float>(i, 5) * frameWidth);
                                     int y2 = (int)(detectionMat.At<float>(i, 6) * frameHeight);
-                                    //DlibDotNet.Point poin1 = new DlibDotNet.Point(x1,y1);
 
                                     Cv2.Rectangle(frame, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 255, 0), 2, LineTypes.Link4);
                                 }
                             }
                             Cv2.ImWrite(finalOutput, frame);
                         }                                
-					}                          
-				}
-			}
+		    }                          
+		}
+	    }
         }
 
     }
