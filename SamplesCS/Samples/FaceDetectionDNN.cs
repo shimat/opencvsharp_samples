ae6bdf4dcb951dd225e83101d1050dab9fea6bc9
shimat <schimatk@gmail.com>
2019-10-30 23:55:16 +0900

    refactor Pose, HandPose and FaceDetectionDNN

--- SamplesCS/Samples/FaceDetectionDNN.cs
+++ SamplesCS/Samples/FaceDetectionDNN.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Runtime.InteropServices;
 using OpenCvSharp;
 using OpenCvSharp.Dnn;
-using SampleBase;
 
 namespace SamplesCS
 {
@@ -10,49 +8,44 @@ namespace SamplesCS
     /// To run this example first download the face model available here:https://github.com/spmallick/learnopencv/tree/master/FaceDetectionComparison/models
     /// Add the files to the bin folder
     /// </summary>
-    class FaceDetectionDNN : ISample
+    internal class FaceDetectionDNN : ISample
     {
         public void Run()
-        {            
-	    string configFile = "deploy.prototxt";
-            string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
-	    string finalOutput = "DetectedFaces.jpg";
-	    var image = "faces.jpg";
-	    
+        {
+            const string configFile = "deploy.prototxt";
+            const string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
+            const string finalOutput = "DetectedFaces.jpg";
+            const string image = "faces.jpg";
+
             // Read sample image
-            var frame = Cv2.ImRead(image);
+            using var frame = Cv2.ImRead(image);
             int frameHeight = frame.Rows;
             int frameWidth = frame.Cols;
-            using (var faceNet = CvDnn.ReadNetFromCaffe(configFile, faceModel)) 
+            using var faceNet = CvDnn.ReadNetFromCaffe(configFile, faceModel);
+            using var blob = CvDnn.BlobFromImage(frame, 1.0, new Size(300, 300),
+                new Scalar(104, 117, 123), false, false);
+            faceNet.SetInput(blob, "data");
+
+            using var detection = faceNet.Forward("detection_out");
+            using var detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F,
+                detection.Ptr(0));
+            for (int i = 0; i < detectionMat.Rows; i++)
             {
-		using (Mat blob = CvDnn.BlobFromImage(frame, 1.0, new OpenCvSharp.Size(300, 300), new Scalar(104, 117, 123), false, false)) 
-		{
-		    faceNet.SetInput(blob, "data");
+                float confidence = detectionMat.At<float>(i, 2);
 
-		    using (var detection = faceNet.Forward("detection_out")) 
-		    {
-			using (Mat detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F, detection.Ptr(0))) 
-			{
-                            for (int i = 0; i < detectionMat.Rows; i++)
-                            {
-                                float confidence = detectionMat.At<float>(i, 2);
+                if (confidence > 0.7)
+                {
+                    int x1 = (int) (detectionMat.At<float>(i, 3) * frameWidth);
+                    int y1 = (int) (detectionMat.At<float>(i, 4) * frameHeight);
+                    int x2 = (int) (detectionMat.At<float>(i, 5) * frameWidth);
+                    int y2 = (int) (detectionMat.At<float>(i, 6) * frameHeight);
 
-                                if (confidence > 0.7)
-                                {
-                                    int x1 = (int)(detectionMat.At<float>(i, 3) * frameWidth);
-                                    int y1 = (int)(detectionMat.At<float>(i, 4) * frameHeight);
-                                    int x2 = (int)(detectionMat.At<float>(i, 5) * frameWidth);
-                                    int y2 = (int)(detectionMat.At<float>(i, 6) * frameHeight);
+                    Cv2.Rectangle(frame, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 255, 0), 2,
+                        LineTypes.Link4);
+                }
+            }
 
-                                    Cv2.Rectangle(frame, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 255, 0), 2, LineTypes.Link4);
-                                }
-                            }
-                            Cv2.ImWrite(finalOutput, frame);
-                        }                                
-		    }                          
-		}
-	    }
+            Cv2.ImWrite(finalOutput, frame);
         }
-
     }
 }
--- SamplesCS/Samples/Pose.cs
+++ SamplesCS/Samples/Pose.cs
@@ -8,59 +8,54 @@ namespace SamplesCS
     /// To run this example first download the pose model available here: https://github.com/CMU-Perceptual-Computing-Lab/openpose/tree/master/models
     /// Add the files to the bin folder
     /// </summary>
-    class Pose : ISample
+    internal class Pose : ISample
     {
         public void Run()
         {
-            const string Model = "pose_iter_160000.caffemodel";
-            const string ModelTxt = "pose_deploy_linevec_faster_4_stages.prototxt";
+            const string model = "pose_iter_160000.caffemodel";
+            const string modelTxt = "pose_deploy_linevec_faster_4_stages.prototxt";
             const string sampleImage = "single.jpeg";
             const string outputLoc = "Output-Skeleton.jpg";
             const int nPoints = 15;
             const double thresh = 0.1;
 
-            int[][] POSE_PAIRS =
+            int[][] posePairs =
             {
-	        new int[] { 0, 1}, new int[] {1, 2}, new int[] {2, 3},
-	        new int[] { 3, 4}, new int[] {1, 5}, new int[] {5, 6},
-	        new int[] { 6, 7}, new int[] {1, 14}, new int[] {14,8}, new int[] {8, 9},
-	        new int[] { 9, 10}, new int[] {14, 11}, new int[] {11,12}, new int[] {12, 13},
+                new[] {0, 1}, new[] {1, 2}, new[] {2, 3},
+                new[] {3, 4}, new[] {1, 5}, new[] {5, 6},
+                new[] {6, 7}, new[] {1, 14}, new[] {14, 8}, new[] {8, 9},
+                new[] {9, 10}, new[] {14, 11}, new[] {11, 12}, new[] {12, 13},
             };
             
-            var image = sampleImage;
-            var frame = Cv2.ImRead(image);
-            Mat frameCopy = frame.Clone();
+            using var frame = Cv2.ImRead(sampleImage);
+            using var frameCopy = frame.Clone();
             int frameWidth = frame.Cols;
             int frameHeight = frame.Rows;
 
-            int inWidth = 368;
-            int inHeight = 368;
+            const int inWidth = 368;
+            const int inHeight = 368;
 
-            var net = CvDnn.ReadNetFromCaffe(ModelTxt, Model);
+            using var net = CvDnn.ReadNetFromCaffe(modelTxt, model);
             net.SetPreferableBackend(Net.Backend.OPENCV);
             net.SetPreferableTarget(Net.Target.CPU);
 
-            Mat inpBlob = CvDnn.BlobFromImage(frame, 1.0 / 255, new OpenCvSharp.Size(inWidth, inHeight), new Scalar(0, 0, 0), false, false);
+            using var inpBlob = CvDnn.BlobFromImage(frame, 1.0 / 255, new Size(inWidth, inHeight), new Scalar(0, 0, 0), false, false);
 
             net.SetInput(inpBlob);
 
-            Mat output = net.Forward();
+            using var output = net.Forward();
             int H = output.Size(2);
             int W = output.Size(3);
 
-            List<OpenCvSharp.Point> points = new List<OpenCvSharp.Point>();
+            var points = new List<Point>();
 
             for (int n = 0; n < nPoints; n++)
             {
                 // Probability map of corresponding body's part.
-                Mat probMap = new Mat(H, W, MatType.CV_32F, output.Ptr(0, n));
+                using var probMap = new Mat(H, W, MatType.CV_32F, output.Ptr(0, n));
+                var p = new Point2f(-1,-1);
 
-                Point2f p = new Point2f(-1,-1);
-                OpenCvSharp.Point maxLoc;
-                double minVal, maxVal;
-                OpenCvSharp.Point minLoc;
-
-                Cv2.MinMaxLoc(probMap, out minVal, out maxVal, out minLoc, out maxLoc);
+                Cv2.MinMaxLoc(probMap, out _, out var maxVal, out _, out var maxLoc);
 
                 var x = (frameWidth * maxLoc.X) / W;
                 var y = (frameHeight * maxLoc.Y) / H;
@@ -72,31 +67,29 @@ namespace SamplesCS
                     p.Y *= (float)frameHeight / H;
 
                     Cv2.Circle(frameCopy, (int)p.X, (int)p.Y, 8, new Scalar(0, 255, 255), -1);
-                    Cv2.PutText(frameCopy, Cv2.Format(n), new OpenCvSharp.Point((int)p.X, (int)p.Y), HersheyFonts.HersheyComplex, 1, new Scalar(0, 0, 255), 2);
+                    Cv2.PutText(frameCopy, Cv2.Format(n), new Point((int)p.X, (int)p.Y), HersheyFonts.HersheyComplex, 1, new Scalar(0, 0, 255), 2);
                 }
 
-                points.Add((OpenCvSharp.Point)p);
+                points.Add((Point)p);
             }
             int nPairs = 14; //(POSE_PAIRS).Length / POSE_PAIRS[0].Length;
 
             for (int n = 0; n < nPairs; n++)
             {
                 // lookup 2 connected body/hand parts
-                Point2f partA = points[POSE_PAIRS[n][0]];
-                Point partB = points[POSE_PAIRS[n][1]];
+                Point partA = points[posePairs[n][0]];
+                Point partB = points[posePairs[n][1]];
 
                 if (partA.X <= 0 || partA.Y <= 0 || partB.X <= 0 || partB.Y <= 0)
                     continue;
 
-                Cv2.Line(frame, (OpenCvSharp.Point)partA, (OpenCvSharp.Point)partB, new Scalar(0, 255, 255), 8);
-                Cv2.Circle(frame, (int)partA.X, (int)partA.Y, 8, new Scalar(0, 0, 255), -1);
-                Cv2.Circle(frame, (int)partB.X, (int)partB.Y, 8, new Scalar(0, 0, 255), -1);
+                Cv2.Line(frame, partA, partB, new Scalar(0, 255, 255), 8);
+                Cv2.Circle(frame, partA.X, partA.Y, 8, new Scalar(0, 0, 255), -1);
+                Cv2.Circle(frame, partB.X, partB.Y, 8, new Scalar(0, 0, 255), -1);
             }
 			
             var finalOutput = outputLoc;
             Cv2.ImWrite(finalOutput, frame);
-        
         }
-
     }
 }
