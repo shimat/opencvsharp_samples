f8970d3f22a6963f8aff920c2977a7f4cdc30404
Vinicius Silva <vinicio121@hotmail.com>
2019-10-30 11:43:16 +0000

    Add changes to FaceDetectionDNN sample

--- SamplesCS/Samples/FaceDetectionDNN.cs
+++ SamplesCS/Samples/FaceDetectionDNN.cs
@@ -14,8 +14,8 @@ namespace SamplesCS
     {
         public void Run()
         {
-            
-			string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
+            string finalOutput = "DetectedFaces.jpg";
+            string faceModel = "res10_300x300_ssd_iter_140000_fp16.caffemodel";
 			string configFile = "deploy.prototxt";
 			var image = "faces.jpg";
             // Read sample image
@@ -32,8 +32,23 @@ namespace SamplesCS
 					{
 						using (Mat detectionMat = new Mat(detection.Size(2), detection.Size(3), MatType.CV_32F, detection.Ptr(0))) 
 						{
-							
-						}                                
+                            for (int i = 0; i < detectionMat.Rows; i++)
+                            {
+                                float confidence = detectionMat.At<float>(i, 2);
+
+                                if (confidence > 0.7)
+                                {
+                                    int x1 = (int)(detectionMat.At<float>(i, 3) * frameWidth);
+                                    int y1 = (int)(detectionMat.At<float>(i, 4) * frameHeight);
+                                    int x2 = (int)(detectionMat.At<float>(i, 5) * frameWidth);
+                                    int y2 = (int)(detectionMat.At<float>(i, 6) * frameHeight);
+                                    //DlibDotNet.Point poin1 = new DlibDotNet.Point(x1,y1);
+
+                                    Cv2.Rectangle(frame, new Point(x1, y1), new Point(x2, y2), new Scalar(0, 255, 0), 2, LineTypes.Link4);
+                                }
+                            }
+                            Cv2.ImWrite(finalOutput, frame);
+                        }                                
 					}                          
 				}
 			}
