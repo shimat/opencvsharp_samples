acc14b1945266884dbb9e12283aaeb5305e81d31
叶洪 <yehong@cvte.com>
2021-08-11 09:04:25 +0800

    Add VideoCapture demo of WPF.

---
+++ VideoCaptureWPF/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using System.Threading;
+
+using OpenCvSharp;
+using OpenCvSharp.WpfExtensions;
+
+namespace VideoCaptureWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        private readonly VideoCapture capture;
+        private readonly CascadeClassifier cascadeClassifier;
+
+        private readonly BackgroundWorker bkgWorker;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            capture = new VideoCapture();
+            cascadeClassifier = new CascadeClassifier("haarcascade_frontalface_default.xml");
+
+            bkgWorker = new BackgroundWorker { WorkerSupportsCancellation = true };
+            bkgWorker.DoWork += Worker_DoWork;
+
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            capture.Open(0, VideoCaptureAPIs.ANY);
+            if (!capture.IsOpened())
+            {
+                Close();
+                return;
+            }
+
+            bkgWorker.RunWorkerAsync();
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            bkgWorker.CancelAsync();
+
+            capture.Dispose();
+            cascadeClassifier.Dispose();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var worker = (BackgroundWorker)sender;
+            while (!worker.CancellationPending)
+            {
+                using (var frameMat = capture.RetrieveMat())
+                {
+                    var rects = cascadeClassifier.DetectMultiScale(frameMat, 1.1, 5, HaarDetectionTypes.ScaleImage, new OpenCvSharp.Size(30, 30));
+
+                    foreach (var rect in rects)
+                    {
+                        Cv2.Rectangle(frameMat, rect, Scalar.Red);
+                    }
+
+                    // Must create and use WriteableBitmap in the same thread(UI Thread).
+                    Dispatcher.Invoke(() =>
+                    {
+                        FrameImage.Source = frameMat.ToWriteableBitmap();
+                    });
+                }
+
+                Thread.Sleep(30);
+            }
+        }
+    }
+}
